df881f1e291aec7725051aec0bf80226a409eea6
pankajladhar <ladharpankaj@gmail.com>
2019-03-10 22:12:33 +0530

    MG-05 | Add component certificates component | Pankaj

--- src/Components/Certificates/index.js
+++ src/Components/Certificates/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import img1 from './images/img1.png';
+import img2 from './images/img2.png';
+import img3 from './images/img3.png';
 import './Certificates.scss';
 
 const Certificates = props => {
     return (
         <div className="certificates">
             <div className="container">
+                <h1 className="main-title invert">
+                    <strong>Our</strong> Certificates
+                </h1>
+                <div className="certificates-container">
+                    <img src={img1} alt={img1}/>
+                    <img src={img2} alt={img2}/>
+                    <img src={img3} alt={img3}/>
+                </div>
             </div>
         </div>
     );
