c597e3afff1eef6aa878820a33b08e91c16aa7a5
pankajladhar <ladharpankaj@gmail.com>
2019-03-11 21:08:17 +0530

    MG-00 | Add data for WhoWeAre | Pankaj

--- src/Components/WhoWeAre/index.js
+++ src/Components/WhoWeAre/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import WhoWeAreImg from './images/WhoWeAre.jpg';
+import WhoWeAreData from './../../resources/data/WhoWeAre.js';
 import './WhoWeAre.scss';
 
 const WhoWeAre = props => {
@@ -8,29 +8,28 @@ const WhoWeAre = props => {
         <div className="whoWeAre">
             <div className="container">
                 <h1 className="main-title">
-                    <strong>Who</strong> we are
+                    {WhoWeAreData.title()}
                 </h1>
                 <div className="content">
                     <div className="left__content">
                         <h2>
-                            welcome to our organic fruit - we serve all types of seasonal fruits
+                            {WhoWeAreData.subTitle}
                         </h2>
                         <p>
-                            Cras blandit nibh ut pretium elementum.
-                            Duis bibendum convallis nun cael dictum.
-                            Quisquen ac ipsum porta, ultrices metus sit amet, curs in nisl.
-                            Dui aliquet varius sem sit amet convallis nun ca dictum.
-                            Cras blandit nibh ut pretium elementum.
-                            Duis bibendum convallis nun ca dictum aliquet.
+                            {WhoWeAreData.paragraph}
                         </p>
                         <ul>
-                            <li>Duis Bibendum Convallis Blandit Dictum.</li>
-                            <li>Cras Blandit Nibh Blandit Pretium Dolor Ipsum.</li>
-                            <li>Bibendum Convallis Nun Ca Dictum Elementum Ipsum Elit.</li>
+                            {
+                                WhoWeAreData.listItems.map((item) => {
+                                    return (
+                                        <li key={item}>{item}</li>
+                                    )
+                                })
+                            }
                         </ul>
                     </div>
                     <div className="right__content">
-                        <img src={WhoWeAreImg} alt="who we are" />
+                        <img src={WhoWeAreData.img} alt={WhoWeAreData.alt} />
                     </div>
                 </div>
             </div>
---
+++ src/resources/data/WhoWeAre.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import img from './../images/WhoWeAre/WhoWeAre.jpg';
+
+export default {
+    img: img,
+    alt: "who we are",
+    title: () => <><strong>Who</strong> we are</>,
+    subTitle: "welcome to our organic fruit - we serve all types of seasonal fruits",
+    paragraph: `Cras blandit nibh ut pretium elementum.
+        Duis bibendum convallis nun cael dictum.
+        Quisquen ac ipsum porta, ultrices metus sit amet, curs in nisl.
+        Dui aliquet varius sem sit amet convallis nun ca dictum.
+        Cras blandit nibh ut pretium elementum.
+        Duis bibendum convallis nun ca dictum aliquet.`,
+    listItems: [
+        "Duis Bibendum Convallis Blandit Dictum.",
+        "Cras Blandit Nibh Blandit Pretium Dolor Ipsum.",
+        "Bibendum Convallis Nun Ca Dictum Elementum Ipsum Elit."
+    ]
+
+}
