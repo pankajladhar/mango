e4eca8852d911a87a68e184f63f96dff77c3fcb1
pankajladhar <ladharpankaj@gmail.com>
2019-03-11 22:34:22 +0530

    MG-00 | Add footer component | Pankaj

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import data from './../../resources/data/Footer';
 import './Footer.scss';
 
 const Footer = props => {
     return (
         <footer>
             <div className="container">
-                div.addre
-            </div>  
+                <h1 className="main-title">
+                    {data.title()}
+                </h1>
+                <div className="content">
+                    <div className="left__content">
+                        <p className="line1">{data.address.line1}</p>
+                        <p className="line">{data.address.line2}</p>
+                        <p className="line">{data.address.line3}</p>
+                        <p className="line">{data.address.line4}</p>
+                        <p className="line">{data.address.contactNumber}</p>
+                        <p className="line">{data.address.email}</p>
+                    </div>
+                    <div className="right__content">
+                        <img src={data.img} alt={data.alt} />
+                    </div>
+                </div>
+            </div>
         </footer>
     );
 };
 
 Footer.propTypes = {
-    
+
 };
 
 export default Footer;
---
+++ src/resources/data/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import img1 from "./../images/Footer/map.png";
+
+export default {
+    img: img1,
+    alt: "our address",
+    title: () => <><strong>Our</strong> Address</>,
+    address: {
+        line1: "Marriott Hotel, Whitefield, Bangalore",
+        line2: "8th Road, Plot No 75",
+        line3: "EPIP Area, Whitefield,",
+        line4: "Bengaluru 560066",
+        contactNumber: "+91 8424873789",
+        email: "email@email.com",
+    }
+};
