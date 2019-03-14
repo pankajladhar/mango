0e84002c9083fa0617ee5adf8c421428fb7713cf
pankajladhar <ladharpankaj@gmail.com>
2019-03-14 23:17:52 +0530

    Add form validation

--- src/Components/UserForm/index.js
+++ src/Components/UserForm/index.js
@@ -1,41 +1,97 @@
 import React, { Component } from 'react';
 import Button from './../Button';
-import SimpleReactValidator from './../../simple-react-validator'
+import FormValidator from './FormValidator';
 import { WriteInFirebase } from './../../Firebase';
 import './UserForm.scss';
 
 class UserForm extends Component {
     constructor(props) {
         super(props);
-        this.validator = new SimpleReactValidator();
+        this.validator = new FormValidator([
+            {
+                field: 'cname',
+                method: 'isEmpty',
+                validWhen: false,
+                message: 'Name is required.'
+            },
+            {
+                field: 'email',
+                method: 'isEmpty',
+                validWhen: false,
+                message: 'Email is required.'
+            },
+            {
+                field: 'email',
+                method: 'isEmail',
+                validWhen: true,
+                message: 'That is not a valid email.'
+            },
+            {
+                field: 'phone',
+                method: 'isEmpty',
+                validWhen: false,
+                message: 'Pleave provide a phone number.'
+            },
+            {
+                field: 'phone',
+                method: 'matches',
+                args: [/^\(?\d\d\d\)? ?\d\d\d-?\d\d\d\d$/], // args is an optional array of arguements that will be passed to the validation method
+                validWhen: true,
+                message: 'That is not a valid phone number.'
+            },
+            {
+                field: 'address',
+                method: 'isEmpty',
+                validWhen: false,
+                message: 'Please provide delivery address.'
+            },
+        ]);
+
         this.state = {
-            cname: ''
+            cname: '',
+            email: '',
+            phone: '',
+            address: '',
+            validation: this.validator.valid()
         }
+
+        this.submitted = false;
+
     }
 
-    handleClick = () => {
-        // if (this.validator.allValid()) {
+    handleInputChange = event => {
+        event.preventDefault();
+
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    handleFormSubmit = () => {
+        const validation = this.validator.validate(this.state);
+        this.setState({ validation });
+        this.submitted = true;
+
+        if (validation.isValid) {
             let data = {
                 selectedProduct: "1 dozen",
-                name: "Pankaj Ladhar",
-                email: "ladharpankaj@gmail.com",
-                contactNumber: "8147311189",
-                address: "Bangalore",
+                name: this.state.cname,
+                email: this.state.email,
+                contactNumber: this.state.phone,
+                address: this.state.address,
             }
             WriteInFirebase(data, "customers").then(() => {
                 alert('You submitted the form and stuff!');
             })
-        // } else {
-        //     this.validator.showMessages();
-        //     this.forceUpdate();
-        // }
+        }
     }
 
-    handleChange = (e) => {
-        this.setState({ cname: e.target.value });
-    }
+
 
     render() {
+        let validation = this.submitted ?                         // if the form has been submitted at least once
+            this.validator.validate(this.state) :   // then check validity every time we render
+            this.state.validation
         return (
             <div className="userForm">
                 <h3>Enter you details</h3>
@@ -58,10 +114,10 @@ class UserForm extends Component {
                         name="cname"
                         id="cname"
                         value={this.state.cname}
-                        onChange={this.handleChange}
+                        onChange={this.handleInputChange}
                         placeholder="Please enter your name"
-                        className="form-control" />
-                    {this.validator.message('name', this.state.cname, 'required')}
+                        className={`form-control ${validation.cname.isInvalid && 'error'}`} />
+                    <label className="has-error">{validation.cname.message}</label>
                 </div>
                 <div className="form-group">
                     <label htmlFor="emailid">Email
@@ -70,8 +126,9 @@ class UserForm extends Component {
                         name="email"
                         id="emailid"
                         placeholder="Please enter your email"
-                        className="form-control" />
-                    {/* {this.validator.message('email', this.state.cname, 'required|email')} */}
+                        onChange={this.handleInputChange}
+                        className={`form-control ${validation.email.isInvalid && 'error'}`} />
+                    <label className="has-error">{validation.email.message}</label>
                 </div>
                 <div className="form-group">
                     <label htmlFor="phone">Contact number
@@ -80,20 +137,24 @@ class UserForm extends Component {
                     <input type="tel"
                         name="phone"
                         id="phone"
+                        onChange={this.handleInputChange}
                         placeholder="Please enter your contact number"
-                        className="form-control" />
+                        className={`form-control ${validation.phone.isInvalid && 'error'}`} />
+                    <label className="has-error">{validation.phone.message}</label>
                 </div>
                 <div className="form-group">
                     <label htmlFor="address">Delivery address
                         <span className="asterisk">*</span></label>
                     <textarea id="address"
-                        name="adress"
+                        name="address"
+                        onChange={this.handleInputChange}
                         placeholder="Please enter your address"
-                        className="form-control">
+                        className={`form-control ${validation.phone.isInvalid && 'error'}`} >
                     </textarea>
+                    <label className="has-error">{validation.address.message}</label>
                 </div>
                 <div className="btn-container">
-                    <Button title="Save Details" onClick={this.handleClick} />
+                    <Button title="Save Details" onClick={this.handleFormSubmit} />
                 </div>
                 <div className="close-btn" onClick={this.props.hideMenu}>x</div>
             </div>
